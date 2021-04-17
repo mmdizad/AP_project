6a29a0646b390c8e6b8270d1ea4c86f635495c08
rezavahidi <79265376+rezavahidi@users.noreply.github.com>
2021-04-17 06:35:53 +0430

    Delete src directory

--- src/Spell.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class Spell extends Card{
-
-    public Spell(String name,String description,String type,int price,String category){
-        super(name,description,type,price,category);
-    }
-
-}
