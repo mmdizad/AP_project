a02aceba52b46933d6fc6703bcea19905e1c88b6
rezavahidi <79265376+rezavahidi@users.noreply.github.com>
2021-04-17 20:03:53 +0430

    Delete src/Model directory

--- src/Model/Trap.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class Trap extends Card{
-
-    public Trap(String name,String description,String type,int price,String category){
-        super(name,description,type,price,category);
-    }
-
-}
