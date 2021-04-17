5860046fd2fc3fb6b1bd774ce273c221b2d870d9
mmd <mmdizadi@yahoo.com>
2021-04-17 22:36:06 +0430

    Model and Controller commited.

---
+++ src/View/MainPhase1Controller.java
@@ -0,0 +1,56 @@
+package View;
+
+import java.util.regex.Matcher;
+
+public class MainPhase1View extends DuelView implements Set,Summon{
+    @Override
+    public void run() {
+        super.run();
+    }
+
+
+    @Override
+    public void summon() {
+
+    }
+
+    @Override
+    public void flipSummon() {
+
+    }
+
+    @Override
+    public void specialSummon() {
+
+    }
+
+    @Override
+    public void ritualSummon() {
+
+    }
+
+    @Override
+    public void set() {
+
+    }
+
+    @Override
+    public void setTrap() {
+
+    }
+
+    @Override
+    public void setSpell() {
+
+    }
+
+    @Override
+    public void setMonster() {
+
+    }
+
+    @Override
+    public void setPosition() {
+
+    }
+}
---
+++ src/View/MainPhase2View.java
@@ -0,0 +1,36 @@
+package View;
+
+import java.util.regex.Matcher;
+
+public class MainPhase2View extends DuelView implements Set{
+    @Override
+    public void run() {
+        super.run();
+    }
+
+
+    @Override
+    public void set() {
+
+    }
+
+    @Override
+    public void setTrap() {
+
+    }
+
+    @Override
+    public void setSpell() {
+
+    }
+
+    @Override
+    public void setMonster() {
+
+    }
+
+    @Override
+    public void setPosition() {
+
+    }
+}
