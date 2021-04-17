a9fb11f2218671d94ae836c58913ec217ea28e23
mmd <mmdizadi@yahoo.com>
2021-04-17 20:56:36 +0430

    Model and Controller commited.

---
+++ src/View/LoginView.java
@@ -0,0 +1,24 @@
+package View;
+
+import java.awt.*;
+import java.util.regex.Matcher;
+
+public class LoginView extends Menu {
+    public void run(){
+
+    }
+    public void createUser(Matcher matcher){
+
+    }
+
+    public void login(Matcher matcher){
+
+    }
+    public Matcher getCommandMatcher(){
+
+    }
+    public void showMenu(Matcher matcher){
+
+    }
+
+}
