1e075455be8f37f54e3c90353632942d8f98f076
mmd <mmdizadi@yahoo.com>
2021-04-17 20:57:54 +0430

    Model and Controller commited.

---
+++ src/View/MainMenu.java
@@ -0,0 +1,18 @@
+package View;
+
+import java.util.regex.Matcher;
+
+public class MainMenu {
+    public void run() {
+    }
+
+    protected void showMenu(String command) {
+    }
+
+    public void enterMenu(String command) {
+    }
+
+    protected Matcher getCommandMatcher(String command, String regex) {
+    }
+
+}
