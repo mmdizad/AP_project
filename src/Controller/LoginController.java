cee6b491f07250673f0824e211698c3653588cb6
mmd <mmdizadi@yahoo.com>
2021-04-19 03:07:52 +0430

    login user committed(without create cards).

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -6,18 +6,25 @@ public class LoginController {
     public User user;
 
     public String createUser(String username, String nickname, String password) {
-        if (User.isUserWithThisUsernameExists(username)){
-            return "user with username " +username+ " already exists";
-        }else if (User.isUserWithThisNicknameExists(nickname)){
-            return "user with nickname "+nickname+" already exists";
-        }else {
-            User newUser = new User(username,nickname,password);
+        if (User.isUserWithThisUsernameExists(username)) {
+            return "user with username " + username + " already exists";
+        } else if (User.isUserWithThisNicknameExists(nickname)) {
+            return "user with nickname " + nickname + " already exists";
+        } else {
+            User newUser = new User(username, nickname, password);
             return "user created successfully!";
         }
     }
 
-    public String login(String command) {
-        return null;
+    public String login(String username, String password) {
+        if (!User.isUserWithThisUsernameExists(username)) {
+            return "Username and password didn't match!";
+        } else if (!User.getUserByUsername(username).getPassword().equals(password)) {
+            return "Username and password didn't match!";
+        } else {
+            user = User.getUserByUsername(username);
+            return "user logged in successfully!";
+        }
     }
 
     public void createCard() {
--- src/View/LoginView.java
+++ src/View/LoginView.java
@@ -9,26 +9,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginView extends Menu {
+    private boolean invalidCommand;
+
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            invalidCommand = true;
             String input = scanner.nextLine();
-            isCreateUserParametersValid(getCommandMatcher(input,"^user create (\\S+) (\\S+) " +
+            isCreateUserParametersValid(getCommandMatcher(input, "^user create (\\S+) (\\S+) " +
                     "(\\S+) (\\S+) (\\S+) (\\S+)$"));
-            showMenu(getCommandMatcher(input,"menu show-current"));
+            isLoginParametersValid(getCommandMatcher(input, "^user login (\\S+) (\\S+) (\\S+)" +
+                    " (\\S+)$"));
+            showMenu(getCommandMatcher(input, "^menu show-current$"));
             if (input.equals("menu exit")) {
                 break;
+            }
+            if (invalidCommand) {
+                System.out.println("invalid command");
             }
         }
     }
 
     public void createUser(String username, String nickname, String password) {
         LoginController loginController = new LoginController();
-        System.out.println(loginController.createUser(username,nickname,password));
+        System.out.println(loginController.createUser(username, nickname, password));
     }
 
-    public void login(Matcher matcher) {
-
+    public void login(String username, String password) {
+        LoginController loginController = new LoginController();
+        System.out.println(loginController.login(username, password));
     }
 
     public Matcher getCommandMatcher(String input, String regex) {
@@ -38,13 +47,15 @@ public class LoginView extends Menu {
     }
 
     public void showMenu(Matcher matcher) {
-        if (matcher.find()){
+        if (matcher.find()) {
+            invalidCommand = false;
             System.out.println("Login Menu");
         }
     }
 
-    public void isCreateUserParametersValid(Matcher matcher){
-        if (matcher.find()){
+    public void isCreateUserParametersValid(Matcher matcher) {
+        if (matcher.find()) {
+            invalidCommand = false;
             int validParameter = 1;
             ArrayList<String> parameters = new ArrayList<>();
             parameters.add(matcher.group(1));
@@ -58,35 +69,67 @@ public class LoginView extends Menu {
                     break;
                 }
             }
-            if (validParameter==1){
-                isCreateUserParametersUnique(matcher,parameters);
+            if (validParameter == 1) {
+                isCreateUserParametersUnique(matcher, parameters);
             }
         }
     }
 
-    public void isCreateUserParametersUnique(Matcher matcher,ArrayList<String> parameters) {
+    public void isCreateUserParametersUnique(Matcher matcher, ArrayList<String> parameters) {
         String parameter1 = parameters.get(0);
         String parameter2 = parameters.get(1);
         String parameter3 = parameters.get(2);
         if (parameter1.equals("-p") && parameter2.equals("-u")
-           && parameter3.equals("-n")){
-              createUser(matcher.group(4),matcher.group(6),matcher.group(2));
-        }else if (parameter1.equals("-p") && parameter2.equals("-n")
-            && parameter3.equals("-u")){
-            createUser(matcher.group(6),matcher.group(4),matcher.group(2));
-        }else if (parameter1.equals("-u") && parameter2.equals("-p")
-                && parameter3.equals("-n")){
-            createUser(matcher.group(2),matcher.group(6),matcher.group(4));
-        }else if (parameter1.equals("-u") && parameter2.equals("-n")
-                && parameter3.equals("-p")){
-            createUser(matcher.group(2),matcher.group(4),matcher.group(6));
-        }else if (parameter1.equals("-n") && parameter2.equals("-p")
-                && parameter3.equals("-u")){
-            createUser(matcher.group(6),matcher.group(2),matcher.group(4));
-        }else if (parameter1.equals("-n") && parameter2.equals("-u")
-                && parameter3.equals("-p")){
-            createUser(matcher.group(4),matcher.group(2),matcher.group(6));
-        }else {
+                && parameter3.equals("-n")) {
+            createUser(matcher.group(4), matcher.group(6), matcher.group(2));
+        } else if (parameter1.equals("-p") && parameter2.equals("-n")
+                && parameter3.equals("-u")) {
+            createUser(matcher.group(6), matcher.group(4), matcher.group(2));
+        } else if (parameter1.equals("-u") && parameter2.equals("-p")
+                && parameter3.equals("-n")) {
+            createUser(matcher.group(2), matcher.group(6), matcher.group(4));
+        } else if (parameter1.equals("-u") && parameter2.equals("-n")
+                && parameter3.equals("-p")) {
+            createUser(matcher.group(2), matcher.group(4), matcher.group(6));
+        } else if (parameter1.equals("-n") && parameter2.equals("-p")
+                && parameter3.equals("-u")) {
+            createUser(matcher.group(6), matcher.group(2), matcher.group(4));
+        } else if (parameter1.equals("-n") && parameter2.equals("-u")
+                && parameter3.equals("-p")) {
+            createUser(matcher.group(4), matcher.group(2), matcher.group(6));
+        } else {
+            System.out.println("invalid command");
+        }
+    }
+
+    public void isLoginParametersValid(Matcher matcher) {
+        if (matcher.find()) {
+            invalidCommand = false;
+            int validParameter = 1;
+            ArrayList<String> parameters = new ArrayList<>();
+            parameters.add(matcher.group(1));
+            parameters.add(matcher.group(3));
+            for (String parameter : parameters) {
+                if (!parameter.equals("-p") && !parameter.equals("-u")) {
+                    System.out.println("invalid command");
+                    validParameter = 0;
+                    break;
+                }
+            }
+            if (validParameter == 1) {
+                isLoginParametersUnique(matcher, parameters);
+            }
+        }
+    }
+
+    public void isLoginParametersUnique(Matcher matcher, ArrayList<String> parameters) {
+        String parameter1 = parameters.get(0);
+        String parameter2 = parameters.get(1);
+        if (parameter1.equals("-p") && parameter2.equals("-u")) {
+            login(matcher.group(4), matcher.group(2));
+        } else if (parameter1.equals("-u") && parameter2.equals("-p")) {
+            login(matcher.group(2), matcher.group(4));
+        } else {
             System.out.println("invalid command");
         }
     }
