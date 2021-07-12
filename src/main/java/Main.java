d33eb3c09f40d9f9dd8852849892b37d813252c2
mmd <mmdizadi@yahoo.com>
2021-07-12 14:20:57 +0430

    introduction of phase 3 committed...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,54 @@
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        runApp();
+    }
 
+    public static void runApp() throws IOException {
+        // TODO ServerController.loadData();
+
+        ServerSocket serverSocket = new ServerSocket(7777);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            startNewThread(serverSocket, socket);
+        }
+    }
+
+    private static void startNewThread(ServerSocket serverSocket, Socket socket) {
+        new Thread(() -> {
+            try {
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                getInputAndProcess(dataInputStream, dataOutputStream);
+                socket.close();
+                serverSocket.close();
+                dataInputStream.close();
+            } catch (IOException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private static void getInputAndProcess(DataInputStream dataInputStream, DataOutputStream dataOutputStream) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        while (true) {
+            String input = dataInputStream.readUTF();
+            String response = process(input);
+            if (input.equals(""))
+                break;
+            else {
+                dataOutputStream.writeUTF(response);
+                dataOutputStream.flush();
+            }
+        }
+    }
+
+    private static String process(String input) {
+             return "";
     }
 }
