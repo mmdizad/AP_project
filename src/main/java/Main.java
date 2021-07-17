66cc6e90d55993c4ac911f7fd2f856bdd488be91
erfanasd <erfan.asadi2272@gmail.com>
2021-07-18 00:17:48 +0530

    shop debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        LoginAndSignUpController.createFolders();
 //        LoginAndSignUpController.createCard();
+        ShopController.initializeCard();
         runApp();
     }
 
